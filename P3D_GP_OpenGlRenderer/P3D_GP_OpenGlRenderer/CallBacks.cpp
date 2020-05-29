a506e3f6f1e210ede3a45b01e6cb250cb58122e2
Eurico Martins <euricomartinss@gmail.com>
2020-05-29 20:20:01 +0100

    Clean older files (unused)

--- P3D_GP_OpenGlRenderer/P3D_GP_OpenGlRenderer/CallBacks.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "CallBacksDeff.h"
-
-// utilizaçao do namespace
-namespace P3D
-{
-	
-}
--- P3D_GP_OpenGlRenderer/P3D_GP_OpenGlRenderer/CallBacksDeff.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-
-namespace P3D
-{
-	// callback para imprimir erros de execuçao
-	void DisplayErrorCallback(int error, const char* desc);
-}
